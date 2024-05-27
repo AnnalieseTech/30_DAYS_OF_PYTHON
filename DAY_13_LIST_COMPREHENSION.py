2792ce61a8c34d35ff26ec267bc36d57211e0bda
AnnalieseTech <annaliesetech@gmail.com>
2024-05-26 20:15:43 -0400

    Update DAY_13_LIST_COMPREHENSION.py

--- DAY_13_LIST_COMPREHENSION.py
+++ DAY_13_LIST_COMPREHENSION.py
@@ -8,10 +8,10 @@
 # For instance if you want to change a string to a list of characters. You can use a couple of methods. Let's see some of them:
 
 # # One way
-# language = 'Python'
-# lst = list(language) # changing the string to list
-# print(type(lst))     # list
-# print(lst)           # ['P', 'y', 't', 'h', 'o', 'n']
+language = 'Python'
+lst = list(language) # changing the string to list
+print(type(lst))     # list
+print(lst)           # ['P', 'y', 't', 'h', 'o', 'n']
 
 # # Second way: list comprehension
 # lst = [i for i in language]
