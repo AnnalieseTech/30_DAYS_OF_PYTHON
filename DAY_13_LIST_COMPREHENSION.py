f2188a7817e4ab4491f5b8b91faee96d622c0169
AnnalieseTech <annaliesetech@gmail.com>
2024-05-28 17:37:53 -0400

    Update DAY_13_LIST_COMPREHENSION.py

--- DAY_13_LIST_COMPREHENSION.py
+++ DAY_13_LIST_COMPREHENSION.py
@@ -27,12 +27,12 @@ numbers = [i for i in range(11)]  # to generate numbers from 0 to 10
 print(numbers)                    # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # # It is possible to do mathematical operations during iteration
-# squares = [i * i for i in range(11)]
-# print(squares)                    # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+squares = [i * i for i in range(11)]
+print(squares)                    # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 # # It is also possible to make a list of tuples
-# numbers = [(i, i * i) for i in range(11)]
-# print(numbers)                             # [(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
+numbers = [(i, i * i) for i in range(11)]
+print(numbers)                             # [(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
 # Example:2
 
 # List comprehension can be combined with if expression
