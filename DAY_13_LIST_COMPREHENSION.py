1d42ca9abf3e67a5233e537d3d1eb38142d3ded4
AnnalieseTech <annaliesetech@gmail.com>
2024-06-04 21:22:47 -0400

    Update DAY_13_LIST_COMPREHENSION.py

--- DAY_13_LIST_COMPREHENSION.py
+++ DAY_13_LIST_COMPREHENSION.py
@@ -62,10 +62,10 @@ print(flattened_list)    # [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # To create a lambda function we use lambda keyword followed by a parameter(s), followed by an expression. See the syntax and the example below. Lambda function does not use return but it explicitly returns the expression.
 
 # syntax
-# x = lambda param1, param2, param3: param1 + param2 + param2
-# print(x(arg1, arg2, arg3))
-# Example:
+x = lambda param1, param2, param3: param1 + param2 + param2
+print(x(arg1, arg2, arg3))
 
+# Example:
 # # Named function
 def add_two_nums(a, b):
     return a + b
@@ -73,16 +73,16 @@ def add_two_nums(a, b):
 print(add_two_nums(2, 3))     # 5
 
 # Lets change the above function to a lambda function
-# add_two_nums = lambda a, b: a + b
-# print(add_two_nums(2,3))    # 5
+add_two_nums = lambda a, b: a + b
+print(add_two_nums(2,3))    # 5
 
 # # Self invoking lambda function
-# (lambda a, b: a + b)(2,3) # 5 - need to encapsulate it in print() to see the result in the console
+(lambda a, b: a + b)(2,3) # 5 - need to encapsulate it in print() to see the result in the console
 
-# square = lambda x : x ** 2
-# print(square(3))    # 9
-# cube = lambda x : x ** 3
-# print(cube(3))    # 27
+square = lambda x : x ** 2
+print(square(3))    # 9
+cube = lambda x : x ** 3
+print(cube(3))    # 27
 
 # # Multiple variables
 # multiple_variable = lambda a, b, c: a ** 2 - 3 * b + 4 * c
