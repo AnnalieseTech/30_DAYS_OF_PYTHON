b745e9845f6167feb80dbf7421c5a5a32036e151
AnnalieseTech <annaliesetech@gmail.com>
2024-06-05 17:04:55 -0400

    Delete Comments in DAY_13_LIST_COMPREHENSION.py

--- DAY_13_LIST_COMPREHENSION.py
+++ DAY_13_LIST_COMPREHENSION.py
@@ -90,13 +90,14 @@ print(cube(3))    # 27
 # Lambda Function Inside Another Function
 # Using a lambda function inside another function.
 
-# def power(x):
-#     return lambda n : x ** n
+def power(x):
+    return lambda n : x ** n
 
-# cube = power(2)(3)   # function power now need 2 arguments to run, in separate rounded brackets
-# print(cube)          # 8
-# two_power_of_five = power(2)(5) 
-# print(two_power_of_five)  # 32
+cube = power(2)(3)   # function power now need 2 arguments to run, in separate rounded brackets
+print(cube)          # 8
+two_power_of_five = power(2)(5) 
+print(two_power_of_five)  # 32
+
 # 🌕 Keep up the good work. Keep the momentum going, the sky is the limit! You have just completed day 13 challenges and you are 13 steps a head in to your way to greatness. Now do some exercises for your brain and muscles.
 
 # Exercises: Day 13
@@ -118,22 +119,23 @@ print("Flattened list:", flattened_lists)
 
 
 # output
-# [1, 2, 3, 4, 5, 6, 7, 8, 9]
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 # Using list comprehension create the following list of tuples:
 
-# [(0, 1, 0, 0, 0, 0, 0),
-# (1, 1, 1, 1, 1, 1, 1),
-# (2, 1, 2, 4, 8, 16, 32),
-# (3, 1, 3, 9, 27, 81, 243),
-# (4, 1, 4, 16, 64, 256, 1024),
-# (5, 1, 5, 25, 125, 625, 3125),
-# (6, 1, 6, 36, 216, 1296, 7776),
-# (7, 1, 7, 49, 343, 2401, 16807),
-# (8, 1, 8, 64, 512, 4096, 32768),
-# (9, 1, 9, 81, 729, 6561, 59049),
-# (10, 1, 10, 100, 1000, 10000, 100000)]
+[(0, 1, 0, 0, 0, 0, 0),
+(1, 1, 1, 1, 1, 1, 1),
+(2, 1, 2, 4, 8, 16, 32),
+(3, 1, 3, 9, 27, 81, 243),
+(4, 1, 4, 16, 64, 256, 1024),
+(5, 1, 5, 25, 125, 625, 3125),
+(6, 1, 6, 36, 216, 1296, 7776),
+(7, 1, 7, 49, 343, 2401, 16807),
+(8, 1, 8, 64, 512, 4096, 32768),
+(9, 1, 9, 81, 729, 6561, 59049),
+(10, 1, 10, 100, 1000, 10000, 100000)]
+
 numbers = [(i, 1, i, i**2, i**3, i**4, i**5) for i in range(11)]
 
 print(numbers)
