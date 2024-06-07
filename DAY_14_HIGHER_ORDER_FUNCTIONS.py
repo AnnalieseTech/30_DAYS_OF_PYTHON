94604a08d45e86c66e43ea659f1d7c320abac69c
Annaliese Tech <annaliesetech@gmail.com>
2024-06-06 20:18:52 -0400

    Create DAY_14_HIGHER_ORDER_FUNCTIONS.py

---
+++ DAY_14_HIGHER_ORDER_FUNCTIONS.py
@@ -0,0 +1,12 @@
+# Higher Order Functions
+# In Python functions are treated as first class citizens, allowing you to perform the following operations on functions:
+
+# A function can take one or more functions as parameters
+# A function can be returned as a result of another function
+# A function can be modified
+# A function can be assigned to a variable
+# In this section, we will cover:
+
+# Handling functions as parameters
+# Returning functions as return value from another functions
+# Using Python closures and decorators
